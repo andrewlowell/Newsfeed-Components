24f2837a427d1c34f3cf05b8b45f13add1d17fd6
andrewlowell <andrewlowellr@gmail.com>
2019-01-30 17:39:50 -0700

    Finish article constructor stretch task.

--- Article/Article.js
+++ Article/Article.js
@@ -18,6 +18,41 @@ class Article {
   }
 }
 
+// *** Create articles stuff ************************************
+
+let titleTest = 'A created article';
+let dateTest = 'December 25th, 1958';
+let paragraphsTest = ['here is some stuff', 'here is some more stuff', 'wow, absolutely incredible all the stuff'];
+
+const createArticle = (title, date, paragraphs) => {
+  let articleTag = document.createElement('div');
+  let titleTag = document.createElement('h2');
+  let titleText = document.createTextNode(title);
+  titleTag.appendChild(titleText);
+  articleTag.classList.add('article');
+  articleTag.appendChild(titleTag);
+  let dateTag = document.createElement('p');
+  let dateText = document.createTextNode(date);
+  dateTag.classList.add('date');
+  dateTag.appendChild(dateText);
+  articleTag.appendChild(dateTag);
+  paragraphs.forEach(e => {
+    let superP = document.createElement('p');
+    let superT = document.createTextNode(e);
+    superP.appendChild(superT);
+    articleTag.appendChild(superP);
+  });
+  let buttonTag = document.createElement('span');
+  let buttonText = document.createTextNode('expand');
+  buttonTag.appendChild(buttonText);
+  buttonTag.classList.add('expandButton');
+  articleTag.appendChild(buttonTag);
+  let articleContainer = document.querySelector('.articles');
+  articleContainer.appendChild(articleTag);
+}
+
+createArticle(titleTest, dateTest, paragraphsTest);
+
 /* START HERE: 
 
 - Select all classes named ".article" and assign that value to the articles variable.  
